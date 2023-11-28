321ab7ec07f6509f1998e95c3879a698b882e45e
AMW <amw@danbots.com>
2023-11-28 13:02:45 +0100

    added documentation

--- iolib/imuLib/imuLib.py
+++ iolib/imuLib/imuLib.py
@@ -1,16 +1,23 @@
 #!/bin/python3
 from adafruit_lsm6ds.lsm6ds3 import LSM6DS3
 import board
+from typing import Dict
 
-# Initialize the I2C and LSM6DS3 sensor
-
-
-class IMU():
+class IMU:
     def __init__(self):
+        """
+        Initializes the IMU class with the I2C and LSM6DS3 sensor.
+        """
         i2c = board.I2C()
         self.sox = LSM6DS3(i2c)
 
-    def get_acc(self):
+    def get_acc(self) -> Dict[str, str]:
+        """
+        Gets the acceleration values from the LSM6DS3 sensor.
+
+        Returns:
+            Dict[str, str]: A dictionary containing acceleration values for x, y, and z axes.
+        """
         acc_state = {
             "x": f"{self.sox.acceleration[0]:.2f}",
             "y": f"{self.sox.acceleration[1]:.2f}",
@@ -18,7 +25,13 @@ class IMU():
         }
         return acc_state
     
-    def get_gyro(self):
+    def get_gyro(self) -> Dict[str, str]:
+        """
+        Gets the gyroscope values from the LSM6DS3 sensor.
+
+        Returns:
+            Dict[str, str]: A dictionary containing gyroscope values for x, y, and z axes.
+        """
         gyro_state = {
             "x": f"{self.sox.gyro[0]:.2f}",
             "y": f"{self.sox.gyro[1]:.2f}",
@@ -26,8 +39,7 @@ class IMU():
         }
         return gyro_state
 
-
-
-imu = IMU()
-acc_value = imu.get_acc()
-print(acc_value["x"])
+if __name__ == "__main__":
+    imu = IMU()
+    acc_value = imu.get_acc()
+    print(acc_value["x"])
