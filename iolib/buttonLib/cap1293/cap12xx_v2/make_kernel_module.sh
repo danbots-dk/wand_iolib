1b5dc67025bab9d200379d3757fff3faaf78c1b1
AMW <amw@danbots.com>
2024-01-18 15:13:14 +0100

    one version for each button

--- iolib/buttonLib/cap1293/cap12xx_v2/make_kernel_module.sh
+++ iolib/buttonLib/cap1293/cap12xx_v2/make_kernel_module.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-dtc -@ -I dts -O dtb -o cap12xx.dtbo cap12xx.dts
-sudo cp cap12xx.dtbo /boot/overlays/
+dtc -@ -I dts -O dtb -o cap1293_0.dtbo cap1293_0.dts
+sudo cp cap1293_0.dtbo /boot/overlays/
+
+dtc -@ -I dts -O dtb -o cap1293_1.dtbo cap1293_1.dts
+sudo cp cap1293_1.dtbo /boot/overlays/
 
 make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
 sudo cp cap12xx.ko  /lib/modules/6.1.21-v7l+/kernel/drivers/input/keyboard/
