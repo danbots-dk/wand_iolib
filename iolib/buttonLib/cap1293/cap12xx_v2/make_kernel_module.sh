c098d03570d038809f3ba5994f507aeefcb13618
AMW <amw@danbots.com>
2024-02-14 08:30:14 +0100

    update to compile process

--- iolib/buttonLib/cap1293/cap12xx_v2/make_kernel_module.sh
+++ iolib/buttonLib/cap1293/cap12xx_v2/make_kernel_module.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-dtc -@ -I dts -O dtb -o cap1293_0.dtbo cap1293_0.dts
-sudo cp cap1293_0.dtbo /boot/overlays/
-
-dtc -@ -I dts -O dtb -o cap1293_1.dtbo cap1293_1.dts
-sudo cp cap1293_1.dtbo /boot/overlays/
+dtc -@ -I dts -O dtb -o cap12xx_pca9542.dtbo cap12xx_pca9542.dts
+sudo cp cap12xx_pca9542.dtbo /boot/overlays/
 
 make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
 sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) modules_install
