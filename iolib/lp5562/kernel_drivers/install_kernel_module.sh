22b3367c3af6805caa1a77ddc1eddf3f992c6abf
amw <AMW>
2024-02-20 15:44:36 +0100

    updates

--- iolib/lp5562/kernel_drivers/install_kernel_module.sh
+++ iolib/lp5562/kernel_drivers/install_kernel_module.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-module_file="lp5562.ko.xz"
-destination="/lib/modules/$(uname -r)/kernel/drivers/input/keyboard/"
+dtc -@ -I dts -O dtb -o /usr/local/lib/wand/iolib/lp5562/kernel_drivers/leds-lp5562.dtbo /usr/local/lib/wand/iolib/lp5562/kernel_drivers/leds-lp5562.dts
+sudo cp /usr/local/lib/wand/iolib/lp5562/kernel_drivers/leds-lp5562.dtbo /boot/overlays/
 
-if [ -e "/lib/modules/$(uname -r)/extra/$module_file" ]; then
-    echo "Module file already exists in /lib/modules/$(uname -r)/extra/, make sure to only have on copy of the driver available"
-else
-    sudo cp "$module_file" "$destination"
-    sudo depmod
-fi
+cd /usr/local/lib/wand/iolib/lp5562/kernel_drivers/ && make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
+cd /usr/local/lib/wand/iolib/lp5562/kernel_drivers/ && sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) modules_install
+cd /usr/local/lib/wand/iolib/lp5562/kernel_drivers/ && sudo depmod
