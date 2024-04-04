94f5e65368a7c0622fde9fdbd84c06cc8b7b3a09
AlexanderM-W <amw@danbots.com>
2024-04-04 08:22:05 +0200

    added docstring docs

--- iolib/buttonLib/button_api_example.py
+++ iolib/buttonLib/button_api_example.py
@@ -2,12 +2,29 @@ from evdev import InputDevice, categorize, ecodes
 import threading
 import time
 
-'''
-This script show cases the button API using /dev/input/eventX.
-The script sets up an interrupt that is triggered by the selected button, and can register a press, long press and release.
-'''
 class Button:
+    """
+    Represents a physical button connected to the system as an input device.
+    
+    This class allows for the detection of button press, long press, and release events through callbacks.
+    
+    Attributes:
+        device (InputDevice): The evdev InputDevice instance representing the physical button.
+        key_to_watch (int): The key code to watch for events.
+    """
+    
     def __init__(self, button_id):
+        """
+        Initializes the Button with a device path based on the provided button ID.
+        
+        Args:
+            button_id (str): A string identifier for the button (e.g., "onoff", "front1", "front2").
+            
+        Raises:
+            FileNotFoundError: If the specified input device does not exist.
+            PermissionError: If the program does not have permission to access the input device.
+        """
+        device_path = ''
         try:
             if button_id == "onoff":
                 device_path = "/dev/input/event2"
@@ -22,80 +39,73 @@ class Button:
         except PermissionError:
             print(f"Error: Insufficient permissions to access {device_path}")
 
-
         self.device = InputDevice(device_path)
 
     def read_input_events(self, press_callback_function, release_callback_function=None, while_pressed_callback_function=None):
+        """
+        Starts listening for input events from the button, triggering callbacks as appropriate.
+        
+        This method sets up and starts a separate thread to listen for press, release, and while-pressed events.
+        
+        Args:
+            press_callback_function (callable): A function to call when the button is pressed.
+            release_callback_function (callable, optional): A function to call when the button is released.
+            while_pressed_callback_function (callable, optional): A function to call repeatedly while the button is pressed.
+        """
+        def input_thread():
+            nonlocal is_pressed
+            for event in self.device.read_loop():
+                if event.type == ecodes.EV_KEY:
+                    key = categorize(event)
+                    if key.scancode == self.key_to_watch:
+                        if event.value == 1:
+                            press_callback_function(categorize(event))
+                            is_pressed = True
+                        elif event.value == 2 and while_pressed_callback_function is not None:
+                            while_pressed_thread = threading.Thread(target=while_pressed_thread_function)
+                            while_pressed_thread.daemon = True
+                            while_pressed_thread.start()
+                        elif event.value == 0 and release_callback_function is not None:
+                            release_callback_function(categorize(event))
+                            is_pressed = False
 
-            
-            # Open the input device
-            
-            #print(f"Connected to {device.name}")
+        def while_pressed_thread_function():
+            nonlocal is_pressed
+            while is_pressed:
+                while_pressed_callback_function()
 
-            # Variable to track the pressed state
-            is_pressed = False
-
-            # Read and print input events in a separate thread
-            def input_thread():
-                nonlocal is_pressed
-                print("input_thread")
-                for event in self.device.read_loop():
-                    if event.type == ecodes.EV_KEY:
-                        key = categorize(event)
-                        if key.scancode == self.key_to_watch:
-                            if event.value == 1:
-                                # Call the press callback function
-                                press_callback_function(categorize(event))
-                                is_pressed = True
-                                # If while_pressed_callback_function is provided, start the while-pressed loop
-                            elif event.value == 2 and while_pressed_callback_function is not None:
-                                while_pressed_thread = threading.Thread(target=while_pressed_thread_function)
-                                while_pressed_thread.daemon = True
-                                while_pressed_thread.start()
-
-                            elif event.value == 0 and release_callback_function is not None:
-                                # Call the release callback function
-                                release_callback_function(categorize(event))
-                                is_pressed = False
-
-            def while_pressed_thread_function():
-                nonlocal is_pressed
-                while is_pressed:
-                    # Call the while-pressed callback function
-                    while_pressed_callback_function()
-
-            # Start the input thread
-            input_thread = threading.Thread(target=input_thread)
-            input_thread.daemon = True  # The thread will exit when the main program exits
-            input_thread.start()
-
-            # Your main program can continue running or do other tasks here
-
-            # Optionally, wait for the input thread to finish (e.g., using input_thread.join())
-
+        is_pressed = False
+        input_thread = threading.Thread(target=input_thread)
+        input_thread.daemon = True
+        input_thread.start()
 
 def handle_press_event(event):
-    # Callback function to handle press events
+    """
+    Handles button press events.
+    
+    Args:
+        event: The event object representing the button press.
+    """
     print(f"Press: {event}")
 
 def handle_release_event(event):
-    # Callback function to handle release events
+    """
+    Handles button release events.
+    
+    Args:
+        event: The event object representing the button release.
+    """
     print(f"Release: {event}")
 
 def while_pressed_event():
-    # Callback function to run while the button is pressed
+    """
+    A callback function executed continuously while the button is pressed.
+    """
     print("Button is still pressed!")
     time.sleep(0.1)
 
 if __name__ == "__main__":
-    # Specify the path to your input device, e.g., /dev/input/event0
-    device = "front2"
-    button = Button(device)
-    # Pass the press, release, and while-pressed callback functions as arguments
-    #button.read_input_events(press_callback_function=handle_press_event, release_callback_function=handle_release_event, while_pressed_callback_function=while_pressed_event)
-    button.read_input_events(press_callback_function=handle_press_event, release_callback_function = handle_release_event)
+    button = Button("front2")
+    button.read_input_events(press_callback_function=handle_press_event, release_callback_function=handle_release_event)
 
-    # Your main program can continue running or do other tasks here
-
-    # Optionally, wait for user input to keep the program alive
     input("Press Enter to exit...\n")
