f5f1ee843c4ff24f2afbeca0d8ac526261e0f42b
AMW <amw@danbots.com>
2024-04-17 14:05:43 +0200

    stuff

--- iolib/buttonLib/cap1293/cap12xx_v2/kernel_drivers/compile_driver_and_overlay.sh
+++ iolib/buttonLib/cap1293/cap12xx_v2/kernel_drivers/compile_driver_and_overlay.sh
@@ -13,10 +13,10 @@ cd "$SCRIPT_DIR/src" && sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) mod
 if [ "$ARCH" = "aarch64" ]; then
     echo "64-bit system detected"
     dtc -@ -I dts -O dtb -o "$SCRIPT_DIR/binary/v0.6/cap1293.dtbo" "$SCRIPT_DIR/overlay/cap1293.dts"
-    cp /lib/modules/$(uname -r)/extra/lp5562.ko.xz "$SCRIPT_DIR/binary/v0.6"
+    cp /lib/modules/$(uname -r)/updates/cap12xx.ko.xz "$SCRIPT_DIR/binary/v0.6"
 else
     echo "32-bit system detected"
     dtc -@ -I dts -O dtb -o "$SCRIPT_DIR/binary/v0.5/cap1293.dtbo" "$SCRIPT_DIR/overlay/cap1293.dts"
-    cp /lib/modules/$(uname -r)/extra/lp5562.ko.xz "$SCRIPT_DIR/binary/v0.5"
+    cp /lib/modules/$(uname -r)/extra/cap12xx.ko.xz "$SCRIPT_DIR/binary/v0.5"
 fi
  
