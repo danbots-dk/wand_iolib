750cd868241dddbddd53fc01f89482b79aa27a9f
AMW <amw@danbots.com>
2024-04-17 14:32:49 +0200

    directories

--- iolib/buttonLib/cap1293/cap12xx_v2/kernel_drivers/compile_driver_and_overlay.sh
+++ iolib/buttonLib/cap1293/cap12xx_v2/kernel_drivers/compile_driver_and_overlay.sh
@@ -7,15 +7,18 @@ ARCH=$(uname -m)
 
 
 
-cd "$SCRIPT_DIR/src" && make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
-cd "$SCRIPT_DIR/src" && sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) modules_install
+
 
 if [ "$ARCH" = "aarch64" ]; then
     echo "64-bit system detected"
+    cd "$SCRIPT_DIR/src_64bit" && make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
+    cd "$SCRIPT_DIR/src_64bit" && sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) modules_install
     dtc -@ -I dts -O dtb -o "$SCRIPT_DIR/binary/v0.6/cap1293.dtbo" "$SCRIPT_DIR/overlay/cap1293.dts"
     cp /lib/modules/$(uname -r)/updates/cap12xx.ko.xz "$SCRIPT_DIR/binary/v0.6"
 else
     echo "32-bit system detected"
+    cd "$SCRIPT_DIR/src_32bit" && make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
+    cd "$SCRIPT_DIR/src_32bit" && sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) modules_install
     dtc -@ -I dts -O dtb -o "$SCRIPT_DIR/binary/v0.5/cap1293.dtbo" "$SCRIPT_DIR/overlay/cap1293.dts"
     cp /lib/modules/$(uname -r)/extra/cap12xx.ko.xz "$SCRIPT_DIR/binary/v0.5"
 fi
