01a900e01b6ab61e9a96057ffeff7030388d0380
AMW <amw@danbots.com>
2024-04-29 10:08:49 +0200

    unified front touch and local test lp5562

--- iolib/indicatorLedLib/kernel_drivers/compile_driver_and_overlay.sh
+++ iolib/indicatorLedLib/kernel_drivers/compile_driver_and_overlay.sh
@@ -14,6 +14,7 @@ if [ "$ARCH" = "aarch64" ]; then
     cd "$SCRIPT_DIR/src_64bit" && make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
     cd "$SCRIPT_DIR/src_64bit" && sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) modules_install
     dtc -@ -I dts -O dtb -o "$SCRIPT_DIR/binary/v0.6/leds-lp5562.dtbo" "$SCRIPT_DIR/overlay/leds-lp5562.dts"
+    cp $SCRIPT_DIR/binary/v0.6/leds-lp5562.dtbo /boot/overlays/
     cp /lib/modules/$(uname -r)/updates/lp5562.ko.xz "$SCRIPT_DIR/binary/v0.6"
 else
     echo "32-bit system detected"
