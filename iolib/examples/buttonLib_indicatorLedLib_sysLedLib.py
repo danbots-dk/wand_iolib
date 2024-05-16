2176e55c7ff130360cf011ff8bc66526031de407
amw <amw@danbots.com>
2024-05-16 13:17:41 +0200

    replaced global var with get_state fucntion

--- iolib/examples/buttonLib_indicatorLedLib_sysLedLib.py
+++ iolib/examples/buttonLib_indicatorLedLib_sysLedLib.py
@@ -8,11 +8,11 @@ from scanLedLib import ScanLed
 import time
 
 button1 = Button("front1")
+button2 = Button("front2")
 indicatorLED = SysLED()
 
 scanLED = ScanLed()
 
-sysLed_value = 0
 def handle_press_event(event):
     """
     Handles button press events.
@@ -21,14 +21,14 @@ def handle_press_event(event):
         event: The event object representing the button press.
     """
     
-    global sysLed_value
     print(f"Press: {event}")
-    if sysLed_value == 0:
+    r, g, b = indicatorLED.get_state()
+    if r == 0:
         sysLed_value = 255
     else:
         sysLed_value = 0
     
-    indicatorLED.set_brightness(sysLed_value)
+    indicatorLED.set_brightness(sysLed_value, 0, 0)
 
 def handle_release_event(event):
     """
@@ -38,6 +38,7 @@ def handle_release_event(event):
         event: The event object representing the button release.
     """
     scanLED.set_dias(0)
+    scanLED.set_flash(0)
     print(f"Press: {event}")
 
 def while_pressed_event():
@@ -48,9 +49,12 @@ def while_pressed_event():
         event: The event object representing the button press.
     """
     scanLED.set_dias(1)
+    scanLED.set_flash(1)
     print(f"pressed")
     time.sleep(0.1)
 
 button1.read_input_events(press_callback_function=handle_press_event, release_callback_function=handle_release_event, while_pressed_callback_function=while_pressed_event)
+button2.read_input_events(press_callback_function=handle_press_event, release_callback_function=handle_release_event, while_pressed_callback_function=while_pressed_event)
+
 input("Press Enter to exit...\n")
 GPIO.cleanup()
